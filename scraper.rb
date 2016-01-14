aef08d408942927b1fcbe62dcfa36d83d02a1904
Tony Bowden <tony@tmtm.com>
2016-01-14 22:07:22 +0000

    Modernise scraper

--- scraper.rb
+++ scraper.rb
@@ -1,31 +1,14 @@
 #!/bin/env ruby
 # encoding: utf-8
 
-require 'scraperwiki'
 require 'wikidata/fetcher'
-require 'nokogiri'
-require 'open-uri'
 require 'pry'
-require 'rest-client'
 
-def noko_for(url)
-  Nokogiri::HTML(open(url).read) 
-end
+names = EveryPolitician::Wikidata.wikipedia_xpath( 
+  url: 'https://lv.wikipedia.org/wiki/12._Saeimas_deput%C4%81ti',
+  xpath: '//h2[span[contains(.,"Deputātu")]]/following-sibling::table[1]//tr[td]/td[1]//a[not(@class="new")]/@title',
+) 
 
-def wikinames(url)
-  noko = noko_for(url)
-  noko.xpath('//h2[span[contains(.,"Deputātu")]]/following-sibling::table[1]//tr[td]/td[1]//a[not(@class="new")]/@title').map(&:text)
-end
-
-names = wikinames('https://lv.wikipedia.org/wiki/12._Saeimas_deput%C4%81ti')
-
-WikiData.ids_from_pages('lv', names).each_with_index do |p, i|
-  data = WikiData::Fetcher.new(id: p.last).data('lv') rescue nil
-  unless data
-    warn "No data for #{p}"
-    next
-  end
-  ScraperWiki.save_sqlite([:id], data)
-end
-warn RestClient.post ENV['MORPH_REBUILDER_URL'], {} if ENV['MORPH_REBUILDER_URL']
+EveryPolitician::Wikidata.scrape_wikidata(names: { lv: names })
+warn EveryPolitician::Wikidata.notify_rebuilder
 
