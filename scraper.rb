dbee2f85a104faee0b5dd288523751ab9526ce35
Tony Bowden <tony@tmtm.com>
2016-01-15 22:13:51 +0000

    Fetch from footer template

--- scraper.rb
+++ scraper.rb
@@ -5,8 +5,10 @@ require 'wikidata/fetcher'
 require 'pry'
 
 names = EveryPolitician::Wikidata.wikipedia_xpath( 
-  url: 'https://lv.wikipedia.org/wiki/12._Saeimas_deput%C4%81ti',
-  xpath: '//h2[span[contains(.,"Deputātu")]]/following-sibling::table[1]//tr[td]/td[1]//a[not(@class="new")]/@title',
+  url: 'https://lv.wikipedia.org/wiki/Veidne:12._Saeima',
+  xpath: '//li//a[not(@class="new")]/@title',
+  after: '//th[contains(.,"Saskaņa")]',
+  before: '//th[contains(.,"Beigušās pilnvaras")]',
 ) 
 
 EveryPolitician::Wikidata.scrape_wikidata(names: { lv: names })
